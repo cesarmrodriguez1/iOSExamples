ef20b6cb80e333a733cb16ca3cbd508ed2d627a9
Cesar <cesar.m.rodriguez@tec.mx>
2021-09-10 19:44:52 -0500

    Update 10-09-2021

---
+++ 7) Monsters/Monsters/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  Monsters
+//
+//  Created by César on 10/09/21.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: self.view.bounds.size)
+        let skView = view as! SKView
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+    
+}
